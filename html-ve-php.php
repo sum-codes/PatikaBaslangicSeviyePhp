2c038a1d95cdc7ffb14723daab3b9a6ea94034f7
Sumeyye Ece <sumeyyye.ece@hotmail.com>
2023-06-19 16:00:20 +0300

    html ve php ilişkisi

---
+++ html-ve-php.php
@@ -0,0 +1,17 @@
+<?php echo '<!DOCTYPE html>
+<html>
+    <head>
+        <title>Html İle Php</title>
+    </head>
+<body>';?>
+
+<h1>Html İçinde Php Kullanimi</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure est, omnis doloribus vel modi incidunt quidem ipsa magni quod animi error quae sunt. Ullam ducimus voluptatem aspernatur fugit odit dolores.</p>
+ 
+<script>alert('Ben js koduyum')</script>
+<script>alert('<?="Script içindeki php kodu"?>')</script>
+</body>
+<?php 
+echo "php içerisinde html kod kullanimi var."
+?>
+</html>
