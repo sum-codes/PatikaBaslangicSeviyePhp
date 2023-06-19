f5d6d4c4051867aa26b016beca866b4d90754654
Sumeyye Ece <sumeyyye.ece@hotmail.com>
2023-06-19 17:20:08 +0300

    Artimetik operatörler konusu bitti

---
+++ Aritmetik_operatorler.php
@@ -0,0 +1,24 @@
+<?php 
+ echo 7+2;
+ echo "<br>";
+ echo 7-2;
+ echo "<br>";
+ echo 7*2;
+ echo "<br>";
+ echo 7/2;
+ echo "<br>";
+ echo 7%2; //mod
+ echo "<br>";
+echo 2+5/2*3-1*9;
+echo "<br>";
+echo ((2+5)/2)*(3-1)*9; //parantez önceliği var
+echo "<br>";
+$sayi1=10;
+$sayi2=20;
+echo $sayi1+$sayi2; //veya aşağıdaki gibi
+$sayi3=$sayi1+$sayi2; 
+echo "<br>";
+echo $sayi3;
+echo "<br>";
+echo $sayi3/3;
+?>
