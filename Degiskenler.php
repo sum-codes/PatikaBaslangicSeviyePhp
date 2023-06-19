8881fe88e6a795f24dfb2d0e1cf39f6949ddade4
Sumeyye Ece <sumeyyye.ece@hotmail.com>
2023-06-19 16:15:26 +0300

    Değişkenler konusu bitti

---
+++ Degiskenler.php
@@ -0,0 +1,18 @@
+<?php 
+$isim="Şahin Ersever";
+$isim="Fatma Ersever";
+echo $isim;
+echo "<br>";
+$_link="patika.dev";
+echo $_link;
+//$1_link sayıyla başlandığı için hata verir.
+echo "<br>";
+$a=5;
+$A=4;
+echo $a;
+echo "<br>";
+echo $A;
+echo "<br>";
+$şahin='isim';//türkçe karakter kullanılabiliyor.
+echo $şahin;
+?>
