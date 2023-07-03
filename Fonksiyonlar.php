3fced761b18931769f4e984b17964b5efc60f4ac
Sumeyye Ece <sumeyyye.ece@hotmail.com>
2023-07-03 10:45:23 +0300

    Fonksyionlar recursive ve static global eklendi. Konu tamamlandı.

--- Fonksiyonlar.php
+++ Fonksiyonlar.php
@@ -26,9 +26,9 @@ echo"<br>";
 ?>
 <?php
 echo"<p>Anonim Fonksiyonlar</p>";
-$topla= function ($num1, $num2){
+$topla= function ($num1, $num2){ //fonksiyon ismi yok. topla değişkenine atadık.
     return $num1 + $num2;
-};
+}; //semicolon ile sonlandırılır.
 
 echo $topla(1,2) . "<br>";
 ?>
@@ -39,7 +39,7 @@ $topla=function(){
 echo $topla();
 ?>
 <?php
-echo"<br>";
+echo"<br>"."<br>";
 $islem['topla'] = function ($num1, $num2) {
     echo $num1 + $num2.'<br>';
 };
@@ -57,7 +57,7 @@ $islem['topla'](10,5);
 $islem['cikar'](10,5);
 $islem['bol'](10,5);
 $islem['carp'](10,5);
-
+echo"<br>"."veya" ."<br>";
 
 function hesapla($n1, $n2, $islem){
     $islem['topla']($n1,$n2);
@@ -67,31 +67,141 @@ function hesapla($n1, $n2, $islem){
 }
 
 hesapla(10,5, $islem);
+echo"<br>";
 
-$number = [[1,2],[2,3],[3,4],[4,5]];
+echo"<br>"."dizilerde arraymap kullanimi ile"."<br>";
 
-array_map(function ($e){
+$number = [[1,2],[2,3],[3,4],[4,5]];/*e yazarak dizinin bütün elemanlarını işleme tabi tutabilirim. */
+array_map(function ($e){ 
     global $islem;
     echo $e[0].'+'.$e[1].' =';
     $islem['topla']($e[0],$e[1]);
     echo '<br>';
 }, $number);
+echo"<br>"."DİĞER ÖRNEK"."<br>";
+$islem=[
+    "topla"=>function($a,$b){return $a+$b;},
+    "cikar"=>function($a,$b){return $a-$b;},
+    "carp"=>function($a,$b){return $a*$b;},
+    "bol"=>function($a,$b){return $a/$b;}
+];
+echo $islem['topla'](2,4) . "<br>";
+echo $islem["cikar"](2,4) ."<br>";
+echo $islem["carp"](2,4) . "<br>";
+echo $islem["bol"](2,4) . "<br>";
 ?>
+
+
+
 <?php
 echo"<br>" . "------------------------------------------------" . "<br>" ."Recursive Fonksiyonlar";
 
+echo"Recursive (öz yinelemeli) fonksiyonlar yani kendi kendini çagiran fonksiyonlardir.
+Örnek ile açiklayalim:
+categories adinda bir dizi oluşturalim.";
+
+$categories=[
+["id"=>1,
+    "parent"=>0, //üst kategorisi varsa üst kategoriye ait id bilgisi yazılır. Eğer üst kategorisi yoksa 0 yazılır.
+    "name"=>"Bac-End Teknolojileri"    
+],
+[
+"id"=>2,
+"parent"=>0,
+"name"=>"Front-End Teknolojileri"
+],
+[
+    "id"=>3,
+    "parent"=>0,
+    "name"=>"Mobil Uygulama"
+],
+[
+    "id"=>4,
+    "parent"=>1,
+    "name"=>"PHP"
+],
+[
+"id"=>5,
+"parent"=>1,
+"name"=>"NodeJS"
+],
+[
+"id"=>6,
+"parent"=>4,
+"name"=>"Laravel"
+],
+[
+"id"=>7,
+"parent"=>2,
+"name"=>"Codeigniter"
+],
+[
+"id"=>8,
+"parent"=>2,
+"name"=>"Javascript"
+],
+[
+"id"=>9,
+"parent"=>8,
+"name"=>"VueJS"
+],
+[
+"id"=>10,
+"parent"=>8,
+"name"=>"AngularJS"
+]
+];
+echo"<br>"."Bu categori dizisinin elemanlarini hiyararşik biçimde ekrana yazdiralim."."<br>";
+
+
+function categoryList(array $categories, int $parent = 0) : string {
+    $html = '<ul>';
+    foreach ($categories as $category)
+        if ($category['parent'] == $parent):
+            $html .= '<li>' . $category['name'];
+            $html .= categoryList($categories, $category['id']);
+            $html .= '</li>';
+        endif;
+    $html .= '</ul>';
+    return $html;
+}
+
+echo categoryList($categories);
+?>
+
+
+
+<?php
+echo"<br>"."DİĞER ÖRNEK"."<br>";
+function sayi($i){
+    echo $i . "<br>";
+if($i<5)
+{
+    $i++;
+    sayi($i);
+}
+}
+sayi(i:1); //dışarıdan parametre olarak i ye 1 i gönderdik.
 ?>
 <?php
-echo"<br>";
-
+echo"<br>" . "STATIC KAVRAMI" ."<br>";
+function say(){
+   static $sayi=0;
+    echo $sayi;
+    $sayi++;
+}
+say();
+say();
+say();
 ?>
 <?php
-echo"<br>";
-
-?>
-<?php
-echo"<br>";
-
+echo"<br>" ."GLOBAL TANIMI" . "<br>";
+$isim ='Şahin';
+function isimyaz(){
+    global $isim;
+    echo $isim;
+}
+isimyaz();
 ?>
 <?php
 echo"<br>";
