b3de3fd0dd4072e49889105bd995a107b0616efc
Sumeyye Ece <sumeyyye.ece@hotmail.com>
2023-07-07 13:32:01 +0300

    dosya ve dizin taşıma işlemleri

---
+++ dosya-ve-dizin-tasima-isimlendirme.php
@@ -0,0 +1,13 @@
+<?php
+//dosya ve dizinleri taşımak ve isimlendirmek için rename() kullanılır.
+/*mkdir(directory: 'a');
+mkdir(directory:'b');
+*/
+
+//rename(from: 'a', to:'renameA');
+//touch(filename: 'test2.txt');
+//rename(from:'test2.txt', to:'b/renametest.txt');
+
+
+
+?>
