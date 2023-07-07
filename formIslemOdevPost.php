3ee6d246270a7197c390337e44206523f5f38605
Sumeyye Ece <sumeyyye.ece@hotmail.com>
2023-07-07 10:01:43 +0300

    Form İşlemleri Ödevi doğrusu.

--- formIslemOdevPost.php
+++ formIslemOdevPost.php
@@ -1,6 +1,7 @@
 <?php
 
 //print_r($_POST);
+/*
 
 $number = $_POST["number"];
 
@@ -25,4 +26,31 @@ else{
 }
 }
 control($number);
+*/
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Formdan sayı değerini alın
+    $number = $_POST["number"];
+
+    // Girilen değerin boş olup olmadığını kontrol edin
+    if (empty($number)) {
+        echo "Değer boş olamaz.";
+    } else {
+        // 3 ile bölümünden kalanı kontrol eden fonksiyon
+        function kontrolEt($num) {
+            if ($num % 3 == 0) {
+                echo "Girilen değer 3'e bölünebilir.";
+            } else {
+                $kalan = $num % 3;
+                $bölünebilir_değer = $num - $kalan + 3;
+                echo "Girilen değer 3'e bölünemez. En yakin 3'e bölünebilen değer: $bölünebilir_değer";
+            }
+        }
+
+        // Girilen sayıyı kontrol et
+        kontrolEt($number);
+    }
+}
+
+
 ?>
