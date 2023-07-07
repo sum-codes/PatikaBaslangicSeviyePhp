16006057d09467a68b69c49b4517208d592dba28
Sumeyye Ece <sumeyyye.ece@hotmail.com>
2023-07-07 10:07:50 +0300

    form işlemleri

--- formIslemOdevPost.php
+++ formIslemOdevPost.php
@@ -1,31 +1,18 @@
 <?php
 
 //print_r($_POST);
+
 /*
-
 $number = $_POST["number"];
-
 
 function control($number){
 
 if(empty($number)){
     echo"Boş değer girilemez.";
 }
-else{
-    function control($num){
-        if($num % 3 == 0){
-            echo "Girilen değer 3 e bölünebilir.";
-        }
-        else
-        {
-            $remainder = $num % 3;
-            $valueDivisible = $num - $remainder + 3;
-            echo "Girilen değer 3 e bölünemez. En yakin 3 e bölünebilen değer: $valueDivisible";
-        }
-    }
+
 }
-}
-control($number);
+
 */
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
